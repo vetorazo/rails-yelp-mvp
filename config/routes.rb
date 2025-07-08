e0536271149a979bec9bdfbaec6b11730e61110a
Bruno Vetorazo <vetorazo@gmail.com>
2025-07-08 21:56:21 +0900

    Now in the middle of routing, did the create part

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/restaurants", to: "restaurants#index"
+  get "/restaurants/new", to: "restaurants#new"
+  post "/restaurants", to: "restaurants#create"
+  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,38 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create!(
+  [
+    {
+      name: "Sushi Place",
+      address: "123 Tokyo St",
+      phone_number: "123-456-7890",
+      category: "japanese"
+    },
+    {
+      name: "Pasta House",
+      address: "456 Rome Ave",
+      phone_number: "234-567-8901",
+      category: "italian"
+    },
+    {
+      name: "Le Gourmet",
+      address: "789 Paris Rd",
+      phone_number: "345-678-9012",
+      category: "french"
+    },
+    {
+      name: "Waffle World",
+      address: "321 Brussels Blvd",
+      phone_number: "456-789-0123",
+      category: "belgian"
+    },
+    {
+      name: "Dragon Express",
+      address: "654 Beijing Ln",
+      phone_number: "567-890-1234",
+      category: "chinese"
+    }
+  ]
+)
